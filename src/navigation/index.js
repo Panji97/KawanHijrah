b5548e094eafc16007a2e38e2f465b98d94d215f
panji97 <panjisetyokurniawan@gmail.com>
2022-06-15 23:48:42 +0700

    splash screen

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,21 +1,32 @@
-import * as React from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Home, Splash} from '../pages';
+import {MyTabBar} from './MyTabBar';
 
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-function Navigation() {
+const MainApp = () => {
+  return (
+    <Tab.Navigator tabBar={props => <MyTabBar {...props} />}>
+      <Tab.Screen name="Home" component={Home} />
+    </Tab.Navigator>
+  );
+};
+
+const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{headerShown: false}}
-        initialRouteName="Splash">
-        <Stack.Screen name="Home" component={Home} />
+        initialRouteName="MainApp">
         <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="MainApp" component={MainApp} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default Navigation;
